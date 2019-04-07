3e643e5e7f0cae7996208c887e75cd739ea1c8b6
Veronika Dimitrova <veronikadimitrova1035@gmail.com>
2019-04-07 12:00:33 +0300

    second commit

--- lab1/src/product.cpp
+++ lab1/src/product.cpp
@@ -3,3 +3,10 @@ Product::Product(string name, double price){
     this->name = name;
     this->price = price;
 }
+    string Product::getName(){
+        return this->name;
+    }
+    double Product::getPrice()
+    {
+        return this->price;
+    }
